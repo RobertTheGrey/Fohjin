482e1f965fc381bc3892fa5221631ecc526b7690
Robert Greyling <robertgreyling@cozwecan.com>
2009-12-12 19:28:51 +0000

    Moved MVC project to Example

---
+++ Fohjin.DDD.Example/Fohjin.DDD.MVC/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Fohjin.DDD.MVC
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+            RegisterIoC();
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new {controller = "Client", action = "Index", id = ""} // Parameter defaults
+                );
+        }
+
+        private static void RegisterIoC()
+        {
+            MvcApplicationBootStrapper.BootStrap();
+            ControllerBuilder.Current.SetControllerFactory(MvcApplicationBootStrapper.GetControllerFactory());
+        }
+
+
+    }
+}
